4522f67f95aaf32d9f7e7cfc06271679a8362e40
abhiraj646 <abhirajsinghchauhan123@gmail.com>
2025-04-29 19:45:15 +0530

    Update header1.c

--- header1.c
+++ header1.c
@@ -16,18 +16,7 @@ int count(int a){
     for(int i =1;i<=a;i++){ 
         b+=1;
         }
-        if(b==1){
-            return 1;
-        }
-        if(b==2){
-            return 2;
-        }
-        if(b==3){
-            return 3;
-        }
-        if(b==4){
-            return 4;
-        }
+        return b;
         
     }     
     
